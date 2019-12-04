3a6224fee0a1fd20726eebb378db759d7b5acdf0
Jessica394 <55104779+Jessica394@users.noreply.github.com>
2019-12-03 22:48:23 -0500

    Final assignment

---
+++ assignments/final.js
@@ -0,0 +1,7 @@
+/* I got all the coding I didnt know from one place.
+ W3Schools. (2019). How to CSS Code. Retrieved December 3, 2019, from https://www.w3schools.com/cssref/css_colors.asp.
+ This is where I found coding for the color codes, the drop down hover navigation, the order here button and the form/submit button*/
+
+document.getElementById("orderbutton").onclick=function () {location.href= "location.html";}
+
+function myFunction() {alert("The form was submitted");}
